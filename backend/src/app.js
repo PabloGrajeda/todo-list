5d428e6a89cb72c2eef9b12aa7b574ca1eec4b1a
Pablo Grajeda <pablo_082006@hotmail.com>
2025-03-05 20:57:04 -0600

    [Added]: initial routes structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import healthcheckRoutes from './routes/healthcheck.router.js'
+import tasks from './routes/tasks.router.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/v1/healthcheck', healthcheckRoutes)
+app.use('/v1/tasks', tasks)
 
 export default app
---
+++ backend/src/controllers/tasks.controller.js
@@ -0,0 +1,5 @@
+export const getTasks = (req, res) => {
+	return res.json({
+		message: 'fetching tasks...',
+	})
+}
